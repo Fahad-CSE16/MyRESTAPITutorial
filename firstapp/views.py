b2669f69ba049111a62f818bcef30deb2a52d7f4
Fahad-CSE16 <mdfahadhossain>
2021-10-12 19:25:07 +0600

    upto 8th tutorial

---
+++ firstapp/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import render
+
+def homeView(request):
+    return render(request, 'index.html')
+
+@api_view(['GET','POST'])
+def firstAPI(request):
+    if request.method=="POST":
+        name=request.data['name']
+        age=request.data['age']
+        print(name, age)
+        return Response({"name":name,"age":age})
+    context={
+        'name':"Fahad Hossain",
+        'University':"HSTU"
+    }
+    return Response(context)
+    
---
+++ myproject/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from firstapp.views import homeView
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', homeView),
+    path('api/', include('rest_framework.urls')),
+    path('api/firstapp/', include('firstapp.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
