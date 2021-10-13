12a11af2748f627598a07b652b311431beecc4c3
Fahad-CSE16 <mdfahadhossain>
2021-10-13 20:27:15 +0600

    upto jwt based login

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,9 +3,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from firstapp.views import homeView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import MyTokenObtainPairView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeView),
+    path('api/login-api/',obtain_auth_token),
+    path('api/token/',MyTokenObtainPairView.as_view()),
+    path('api/token/refresh/',TokenRefreshView.as_view()),
     path('api/', include('rest_framework.urls')),
     path('api/firstapp/', include('firstapp.urls')),
 ]
